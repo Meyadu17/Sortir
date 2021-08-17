0902a9c3e84cb3aa60b72141fdc3562af14742e1
Meya <melody.delage@gmail.com>
2021-08-17 15:35:49 +0200

    Maj SerieController

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -1,5 +1,49 @@
 <?php
-class SortieController
-{
 
+use App\Form\SortieType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortieController extends AbstractController
+{
+    /**
+     * @Route("/sortie{id}", name="sortie_detail",
+     *     requirements={"id":"\d+"}
+     *     methode={"GET"})
+     */
+
+    public function detail ($id)
+    {
+        $sortieRepo = $this->getDoctrine()->getRepository(Site::class);
+        $sortie = $sortieRepo->find($id);
+        return $this->render('sortie/detail.html.twig', [
+            "sortie"=>$sortie
+        ]);
+    }
+
+    /**
+     * @Route ("/sortie/ajouter", name="sortie_ajouter")
+     */
+    public function ajouter (EntityManagerInterface $em, Request $request)
+    {
+        $sortie = new sortie;
+
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+
+        # Hydratation de l'instance Sortie avec les données qui proviennent de la requête
+        # On utilise handleRequest et on y passe la requête en argument
+        $sortieForm->handleRequest($request);
+        if($sortieForm->isSubmitted() && $sortieForm->isValid())
+        {
+            $em->persist($sortie);
+            $em->flush();
+
+            $this->addFlash('success', 'La sortie à bien été enregistée');
+            return $this->render('sortie/ajouter.html.twig', [
+                "sortieForm"=>$sortieForm->createView()
+            ]);
+        }
+    }
 }
