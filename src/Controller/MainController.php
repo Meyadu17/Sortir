0350faaa18189b87e71096de9296320c882f401a
guillaume <guillaume.lenormand2021@campus-eni.fr>
2021-08-19 09:56:09 +0200

    security

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,14 +7,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 Class MainController extends AbstractController
 {
-    /**
-     * @Route ("/", name="connexion")
-     */
-    public function connexion()
-    {
-
-        return $this->render("default/connexion.html.twig");
-    }
 
     /**
      * @Route ("/accueil.php", name="accueil")
