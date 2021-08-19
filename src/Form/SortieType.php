72293383eb9f925bd4ae7c490ac34fadfc88c759
Jolan <jolan.vandenbroucque@outlook.fr>
2021-08-19 11:34:01 +0200

    Participants dans BDD

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,9 +2,13 @@
 
 namespace App\Form;
 
-use App\Entity\Sortie;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use App\Entity\Sortie;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SortieType extends AbstractType
