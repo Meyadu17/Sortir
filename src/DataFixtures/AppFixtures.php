5f8196b3d9bc1d26adcd410ed75fcc65f9f9fe04
Meya <melody.delage@gmail.com>
2021-08-19 16:01:06 +0200

    maj fixture lieu

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Lieu;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,15 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 20; $i++) {
+            $lieu = new Lieu();
+            $lieu->setNom('Nom ' . $i);
+            $lieu->setrue($i . " allée des Cornouillers");
+            $lieu->setLatitude($i . "00.0128845");
+            $lieu->setLongitude($i . "00.9856758");
 
+            $manager->persist($lieu);
+        }
         $manager->flush();
     }
 }
