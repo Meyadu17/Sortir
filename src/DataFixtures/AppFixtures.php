c2f9aff81bfa65fb7dbced6574d29e8bb3ea88cf
guillaume <guillaume.lenormand2021@campus-eni.fr>
2021-08-20 09:11:16 +0200

    message

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,25 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
+        for ($i = 0; $i < 20; $i++) {
+            $lieu = new Lieu();
+            $lieu->setNom('Nom ' . $i);
+            $lieu->setrue($i . " allée des Cornouillers");
+            $lieu->setLatitude($i . "00.0128845");
+            $lieu->setLongitude($i . "00.9856758");
+
+            $manager->persist($lieu);
+        }
         $manager->flush();
     }
 }
