862cb8c88f32f82fcfa0f9e03ca7ec50e997f69a
Meya <melody.delage@gmail.com>
2021-08-23 12:30:20 +0200

    maj roles

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -32,6 +32,6 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        #Cette methode peut être vide - elle est interprété par le logout key du firewall.');
+        #Cette methode peut être vide - elle est interprété par la clé de déconnexion du firewall.');
     }
 }
